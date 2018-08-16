859e5993d80cea362689094d0c3b5794b0aeb40a
CleetSR388 <41758594+CleetSR388@users.noreply.github.com>
2018-08-16 03:15:02 -0400

    Added the collect reward script and the level system script

---
+++ Assets/Project/Scripts/CollectReward1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectReward : MonoBehaviour {
+
+	[SerializeField]
+	private float experience;
+    public class CollectReward : MonoBehaviour
+    {
+        public bool enemyDefeated;
+
+        // Use this for initialization
+        void Start() {
+            GameObject turnSystem = GameObject.Find("TurnSystem");
+            turnSystem.GetComponent<TurnSystem>().enemyEncounter = gameObject;
+            enemyDefeated = false;
+        }
+
+        public void GetReward() {
+            GameObject[] livingPlayerUnits = GameObject.FindGameObjectsWithTag("PlayerUnit");
+            float experiencePerUnit = experience / livingPlayerUnits.Length;
+            enemyDefeated = true;
+
+
+            foreach (GameObject playerUnit in livingPlayerUnits) {
+                playerUnit.GetComponent<UnitStats>().ReceiveExperience(experiencePerUnit);
+            }
+
+            Destroy(gameObject);
+            public bool IsEnemyDefeated()
+            {
+                return enemyDefeated;
+            }
+        }
+    }
+}
---
+++ Assets/Project/Scripts/LevelSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelSystem : MonoBehaviour
+{
+
+
+    public int XP;
+    public int currentlevel;
+
+    // put the CollectReward script in the respective field in the Unity editor
+    public CollectReward rewardObj;
+
+    void Update()
+    {
+        if (rewardObj.IsEnemyDefeated())
+        {
+            UpdateXp(5);
+        }
+    }
+
+
+    public void UpdateXp(int xp)
+    {
+         XP += xp;
+        }
+
+        int curlvl = (int)(0.1f * Mathf.Sqrt(XP));
+
+        if (curlvl != currentlevel)
+        {
+            currentlevel = curlvl;
+            // add some cool text to show you reached a ew level
+        }
+
+        int xpnextlevel = 100 * (currentlevel * 1);
+        int differencexp = xpnextlevel - XP;
+
+        int totaldifference = xpnextlevel - (100 * currentlevel * currentlevel);
+
+        // differencexp / totaldifference
+    }
+}
