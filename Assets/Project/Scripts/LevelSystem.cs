4eb3f2cefcb304bc1f38f43bef61c28fd0aa10ef
CleetSR388 <41758594+CleetSR388@users.noreply.github.com>
2018-08-21 13:10:25 -0400

    Tried to add an XP bar using a slider

--- Assets/Project/Scripts/LevelSystem.cs
+++ Assets/Project/Scripts/LevelSystem.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 
 
+
+
 public class LevelSystem : MonoBehaviour
 {
     public float XP;
---
+++ Assets/Project/Scripts/UI/ShowUnitXP.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUnitExperience : ShowUnitStat
+{
+
+    protected override float NewStatValue()
+    {
+        return unit.GetComponent<UnitStats>().experience;
+    }
+}
