37fb4cd92624a00863c5efd0818b50a931bbf1d3
CleetSR388 <41758594+CleetSR388@users.noreply.github.com>
2018-08-30 03:02:52 -0400

    I got the fireball to work sounds off too still needs tweaking but hey took long enough to get it to work

--- Assets/Project/Scripts/Attack/PlayerUnitAction.cs
+++ Assets/Project/Scripts/Attack/PlayerUnitAction.cs
@@ -3,49 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerUnitAction : MonoBehaviour {
+public class PlayerUnitAction : MonoBehaviour
+{
 
-	[SerializeField]
-	private GameObject physicalAttackPrefab;
+    [SerializeField]
+    private GameObject physicalAttackPrefab;
 
-	[SerializeField]
-	private GameObject magicalAttackPrefab;
+    [SerializeField]
+    private GameObject magicalAttackPrefab;
+
+    [SerializeField]
+    public GameObject FireballPrefab;
 
     [SerializeField]
     private GameObject usePotionPrefab;
 
     [SerializeField]
-	private Sprite faceSprite;
+    private Sprite faceSprite;
 
-	public GameObject currentAttack;
-	private GameObject physicalAttack;
-	private GameObject magicalAttack;
+    public GameObject currentAttack;
+    private GameObject physicalAttack;
+    private GameObject magicalAttack;
     public GameObject usePotion;
     public GameObject useManaPotion;
     public bool _manaPotion;
     public bool _potion;
     UnitStats unitStats;
 
-	void Awake () {
-		physicalAttack = Instantiate (physicalAttackPrefab, transform);
-		magicalAttack = Instantiate (magicalAttackPrefab, transform);
+    void Awake()
+    {
+        physicalAttack = Instantiate(physicalAttackPrefab, transform);
+        magicalAttack = Instantiate(magicalAttackPrefab, transform);
         usePotion = Instantiate(usePotionPrefab, transform);
 
-		physicalAttack.GetComponent<AttackTarget> ().owner = gameObject;
-		magicalAttack.GetComponent<AttackTarget> ().owner = gameObject;
+        physicalAttack.GetComponent<AttackTarget>().owner = gameObject;
+        magicalAttack.GetComponent<AttackTarget>().owner = gameObject;
 
-		currentAttack = physicalAttack;
-	}
+        currentAttack = physicalAttack;
+    }
 
-	public void Act (GameObject target) {
+    public void Act(GameObject target)
+    {
         if (currentAttack == usePotion)
         {
-           unitStats = this.gameObject.GetComponent<UnitStats>();
-           unitStats.usePotion();
+            unitStats = this.gameObject.GetComponent<UnitStats>();
+            unitStats.usePotion();
             target = null;
             //currentAttack.GetComponent<UsePotion>().Heal(target);
         }
-        else if(currentAttack == useManaPotion)
+        else if (currentAttack == useManaPotion)
         {
             unitStats = this.gameObject.GetComponent<UnitStats>();
             unitStats.useManaPotion();
@@ -55,9 +61,10 @@ public class PlayerUnitAction : MonoBehaviour {
         {
             currentAttack.GetComponent<AttackTarget>().Hit(target);
         }
-	}
+    }
 
-	public void SelectAttack (bool physical, bool potion, bool manaPotion) {
+    public void SelectAttack(bool physical, bool potion, bool manaPotion)
+    {
         if (!potion && !manaPotion)
         {
             currentAttack = physical ? physicalAttack : magicalAttack;
@@ -73,16 +80,29 @@ public class PlayerUnitAction : MonoBehaviour {
             currentAttack = useManaPotion;
             _manaPotion = true;
         }
-	}
+    }
 
-	public void UpdateHUD() {
-		GameObject playerUnitFace = GameObject.Find ("PlayerUnitFace");
-		playerUnitFace.GetComponent<Image> ().sprite = faceSprite;
+    public void UpdateHUD()
+    {
+        GameObject playerUnitFace = GameObject.Find("PlayerUnitFace");
+        playerUnitFace.GetComponent<Image>().sprite = faceSprite;
 
-		GameObject playerUnitHealthBar = GameObject.Find ("PlayerUnitHealthBar");
-		playerUnitHealthBar.GetComponent<ShowUnitHealth> ().ChangeUnit (gameObject);
+        GameObject playerUnitHealthBar = GameObject.Find("PlayerUnitHealthBar");
+        playerUnitHealthBar.GetComponent<ShowUnitHealth>().ChangeUnit(gameObject);
 
-		GameObject playerUnitManaBar = GameObject.Find ("PlayerUnitManaBar");
-		playerUnitManaBar.GetComponent<ShowUnitMana> ().ChangeUnit (gameObject);
-	}
+        GameObject playerUnitManaBar = GameObject.Find("PlayerUnitManaBar");
+        playerUnitManaBar.GetComponent<ShowUnitMana>().ChangeUnit(gameObject);
+    }
+
+    [SerializeField]
+    protected Transform fireballpos;
+
+    public virtual void ThrowFireball(int value)
+    {
+
+        {
+            GameObject tmp = (GameObject)Instantiate(FireballPrefab, fireballpos.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+         
+        }
+    }
 }
