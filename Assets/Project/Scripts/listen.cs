6b4d71392754206a8a527b4ac8b4ab8eedcabfab
CleetSR388 <41758594+CleetSR388@users.noreply.github.com>
2018-08-30 05:17:16 -0400

    Added a bolt projectile for the Knights magical attack

---
+++ Assets/Project/Scripts/listen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class listen : MonoBehaviour {
+
+    AudioListener[] listeners;
+
+    // Use this for initialization
+    void Start() {
+        listeners = FindObjectsOfType<AudioListener>();
+        print(listeners.Length);
+
+        foreach (var listener in listeners)
+        {
+            print(listener.gameObject.name);
+
+
+        }
+    }
+}
+
+
+
